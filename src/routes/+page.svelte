ccb3cdb7b18f64ddd95c8568a278bc4bf1c82a7c
Gavin Popken <gavinpopkenart@gmail.com>
2024-02-25 18:18:06 -0600

    got the DOM to render a list of tasks

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	console.log('hello world');
+    let taskInputValue = ''
+    let tasks = [];
+    $: {
+        console.log('something is being updated!');
+        }
+</script>
+
+<h1>Welcome to Svelte Tasks Manager!</h1>
+
+<label for="taskInput"
+	>New Task:
+	<input type="text" id="taskInput" bind:value={taskInputValue}/>
+</label>
+<button on:click={() => {console.log('click');tasks = [...tasks, taskInputValue];taskInputValue=''}}>Add Task!</button>
+
+<div>
+	{#if tasks.length === 0}
+		<p>There are no tasks!</p>
+	{:else}
+		{#each tasks as task}
+			<p>{task}</p>
+		{/each}
+	{/if}
+</div>
