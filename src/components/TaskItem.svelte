94b779555d4a69a0d7a985fc38b5c8d8dbcbff01
Gavin Popken <gavinpopkenart@gmail.com>
2024-02-25 22:49:42 -0600

    finished MVP incorporating Svelte Materialify

---
+++ src/components/TaskItem.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { MaterialApp, Button } from 'svelte-materialify';
+	export let task;
+    export let i;
+</script>
+
+	<tr>
+		<td>
+			{task}
+		</td>
+		<td>
+			<Button>Mark Complete</Button>
+		</td>
+		<td>
+			<Button class="red white-text" on:click>Delete</Button>
+		</td>
+	</tr>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,47 @@
 <script>
+	import { MaterialApp, Button } from 'svelte-materialify';
+	import TaskItem from '../components/TaskItem.svelte';
 	console.log('hello world');
-    let taskInputValue = ''
-    let tasks = [];
-    $: {
-        console.log('something is being updated!');
-        }
+	let taskInputValue = '';
+	let tasks = [];
+	$: {
+		console.log('something is being updated!');
+	}
 </script>
 
-<h1>Welcome to Svelte Tasks Manager!</h1>
+<MaterialApp>
+	<h3>Welcome to Svelte Tasks Manager!</h3>
 
-<label for="taskInput"
-	>New Task:
-	<input type="text" id="taskInput" bind:value={taskInputValue}/>
-</label>
-<button on:click={() => {console.log('click');tasks = [...tasks, taskInputValue];taskInputValue=''}}>Add Task!</button>
+	<label for="taskInput"
+		>New Task:
+		<input type="text" id="taskInput" bind:value={taskInputValue} />
+	</label>
+	<Button
+		on:click={() => {
+			console.log('click');
+			tasks = [...tasks, taskInputValue];
+			taskInputValue = '';
+		}}>Add Task!</Button
+	>
 
-<div>
-	{#if tasks.length === 0}
-		<p>There are no tasks!</p>
-	{:else}
-		{#each tasks as task}
-			<p>{task}</p>
-		{/each}
-	{/if}
-</div>
+	<div>
+		{#if tasks.length === 0}
+			<p>There are no tasks!</p>
+		{:else}
+			<table>
+				<thead>
+					<tr>
+						<td>Task:</td>
+						<td>Complete?</td>
+						<td>Delete?</td>
+					</tr>
+				</thead>
+				<tbody>
+					{#each tasks as task, i}
+						<TaskItem {task} {i}/>
+					{/each}
+				</tbody>
+			</table>
+		{/if}
+	</div>
+</MaterialApp>
